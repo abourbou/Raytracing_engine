ef9eddbe52dfb9b3c24b94fc44effbbe7cf9be50
abourbou <arthuro6996@live.fr>
2024-05-07 11:07:19 +0200

    WIP [build] Change architecture + add eigen to raytracing engine

---
+++ include/engine/Camera.hpp
@@ -0,0 +1,9 @@
+
+#include <Eigen/Dense>
+
+class Camera
+{
+    public:
+        Camera(Eigen::Vector3d position, Eigen::Vector3d direction);
+    private:
+};
---
+++ src/engine/Camera.cpp
@@ -0,0 +1,7 @@
+
+#include "engine/Camera.hpp"
+
+Camera::Camera(Eigen::Vector3d position, Eigen::Vector3d direction)
+{
+
+}
